370c909b0957bba470664d3836d65d3879a06e26
mahlen <malink027@gmail.com>
2024-10-14 12:23:01 -0400

    linear and binary search

---
+++ linear-search.ts
@@ -0,0 +1,6 @@
+const array = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+// find the letter e in an array of letters
+const linearSearch = (array) => array.find(element => element === 'e');
+
+linearSearch(array);
