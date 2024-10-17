96cff86e0336a2a818b22a9a35a820dd52f7ccbd
mahlen <malink027@gmail.com>
2024-10-17 11:55:46 -0400

    refactored code for fullstack using nextJs and express

--- backend/index.ts
+++ backend/index.ts
@@ -19,14 +19,14 @@ app.listen(port, () => {
 // Routes
 
 // Linear search
-import { linearSearch } from "./algorithms/search/linear-search";
+// import { linearSearch } from "./algorithms/search/linear-search";
 
-app.post("/algos/search/linear", (req, res) => {
-	const node = req.body;
-	console.log(node);
-	const algoOutput = linearSearch(node);
-	res.status(200).json(algoOutput);
-});
+// app.post("/algos/search/linear", (req, res) => {
+// 	const node = req.body;
+// 	//console.log(node);
+// 	const algoOutput = linearSearch(node);
+// 	res.status(200).json(algoOutput);
+// });
 
 // Binary search
 import { binarySearch } from "./algorithms/search/binary-search";
@@ -38,56 +38,56 @@ app.post("/algos/search/binary", (req, res) => {
 	res.status(200).json(algoOutput);
 });
 
-// BFS
+// // BFS
 
-// DFS
+// // DFS
 
 // Bubble sort
-import { BubbleSort } from "./algorithms/sort/bubbleSort";
+// import { BubbleSort } from "./algorithms/sort/bubbleSort";
 
-app.post("/algos/sort/bubble", (req, res) => {
-	const node = req.body;
-	console.log(node);
-	const algoOutput = BubbleSort(node);
-	res.status(200).json(algoOutput);
-});
+// app.post("/algos/sort/bubble", (req, res) => {
+// 	const node = req.body;
+// 	//console.log(node);
+// 	const algoOutput = BubbleSort(node);
+// 	res.status(200).json(algoOutput);
+// });
 
-// Selection sort
-import { selectionSort } from "./algorithms/sort/selectionSort";
+// // Selection sort
+// import { selectionSort } from "./algorithms/sort/selectionSort";
 
-app.post("/algos/sort/selection", (req, res) => {
-	const node = req.body;
-	console.log(node);
-	const algoOutput = selectionSort(node);
-	res.status(200).json(algoOutput);
-});
+// app.post("/algos/sort/selection", (req, res) => {
+// 	const node = req.body;
+// 	console.log(node);
+// 	const algoOutput = selectionSort(node);
+// 	res.status(200).json(algoOutput);
+// });
 
-// Insertion sort
-import { insertionSort } from "./algorithms/sort/insertionSort";
+// // Insertion sort
+// import { insertionSort } from "./algorithms/sort/insertionSort";
 
-app.post("/algos/sort/insertion", (req, res) => {
-	const { list } = req.body;
-	console.log(list);
-	const algoOutput = insertionSort(list);
-	res.status(200).json(algoOutput);
-});
+// app.post("/algos/sort/insertion", (req, res) => {
+// 	const { list } = req.body;
+// 	console.log(list);
+// 	const algoOutput = insertionSort(list);
+// 	res.status(200).json(algoOutput);
+// });
 
-// Merge sort
-import { mergeSort } from "../algorithms/sort/mergeSort";
+// // Merge sort
+// import { mergeSort } from "../algorithms/sort/mergeSort";
 
-app.post("/algos/sort/merge", (req, res) => {
-	const { list } = req.body;
-	console.log(list);
-	const algoOutput = mergeSort(list);
-	res.status(200).json(algoOutput);
-});
+// app.post("/algos/sort/merge", (req, res) => {
+// 	const { list } = req.body;
+// 	console.log(list);
+// 	const algoOutput = mergeSort(list);
+// 	res.status(200).json(algoOutput);
+// });
 
-// Quick sort
-import { quickSort } from "../algorithms/sort/quickSort";
+// // Quick sort
+// import { quickSort } from "../algorithms/sort/quickSort";
 
-app.post("/algos/sort/quick", (req, res) => {
-	const { list } = req.body;
-	console.log(list);
-	const algoOutput = quickSort(list);
-	res.status(200).json(algoOutput);
-});
+// app.post("/algos/sort/quick", (req, res) => {
+// 	const { list } = req.body;
+// 	console.log(list);
+// 	const algoOutput = quickSort(list);
+// 	res.status(200).json(algoOutput);
+// });
